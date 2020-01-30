692c5468fbb5d135fcda910b674ad31005824fdd
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-30 13:15:26 -0700

    Add query method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.query_find(query_parameters)
+    if query_parameters.keys == ["created_at"] || query_parameters.keys == ["updated_at"]
+      item = Item.where(query_parameters).order(:id).first
+    else
+      item = Item.find_by(query_parameters)
+    end
+  end
 end
