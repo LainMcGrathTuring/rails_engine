29603e95053df8565374d1be2bb894449eab54a8
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-30 16:17:27 -0700

    Add conditional for invoice_id in index method

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -14,10 +14,12 @@ class Api::V1::InvoiceItemsController < ApplicationController
   def index
     if params[:item_id]
       item = Item.find(params[:item_id])
-      render json: InvoiceItemSerializer.new(InvoiceItem.where(item_id: item.id))
+      invoice_items = InvoiceItem.where(item_id: item.id)
+    elsif params[:invoice_id]
+      invoice_items = InvoiceItem.where(invoice_id: params[:invoice_id])
     else
       invoice_items = InvoiceItem.all
-      render json: InvoiceItemSerializer.new(invoice_items)
     end
+    render json: InvoiceItemSerializer.new(invoice_items)
   end
 end
