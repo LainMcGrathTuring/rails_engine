c5f231f86e0701a9d4c5af4b56a9132b9b0995e6
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-30 13:15:01 -0700

    refactor item show method to use item model method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def show
     if params[:id] == "find"
-      if request.query_parameters.keys == ["created_at"] || request.query_parameters.keys == ["updated_at"]
-        item = Item.where(request.query_parameters).order(:id).first
-      else
-        item = Item.find_by(request.query_parameters)
-      end
+      item = Item.query_find(request.query_parameters)
     elsif params[:id] == "find_all"
       item = Item.where(request.query_parameters).order(:id)
     else
