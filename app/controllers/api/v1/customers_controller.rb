f5f7b1b2610cd911c1be518df4a1be8199d81de0
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-30 16:33:03 -0700

    add invoice_id conditional to customers_controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -5,6 +5,9 @@ class Api::V1::CustomersController < ApplicationController
       customer = Customer.find_by(request.query_parameters)
     elsif params[:id] == "find_all"
       customer = Customer.where(request.query_parameters)
+    elsif params[:invoice_id]
+      invoice = Invoice.find_by(id: params[:invoice_id])
+      customer = Customer.find_by(id: invoice.customer_id)
     else
       customer = Customer.find(params[:id])
     end
