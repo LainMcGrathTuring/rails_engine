5c025445f0fcf2b090983fd54a0f6f623f0c2f72
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-29 21:56:13 -0700

    Add index method to invoice controller, update show method to include find

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,8 +1,22 @@
 class Api::V1::InvoicesController < ApplicationController
 
   def index
-    merchant = Merchant.find(params[:merchant_id])
-    render json: InvoiceSerializer.new(merchant.invoices.all)
+    if params[:merchant_id]
+      merchant = Merchant.find(params[:merchant_id])
+      render json: InvoiceSerializer.new(merchant.invoices.all)
+    else
+      render json: InvoiceSerializer.new(Invoice.all)
+    end
   end
 
+  def show
+    if params[:id] == "find"
+      invoice = Invoice.find_by(request.query_parameters)
+    elsif params[:id] == "find_all"
+      invoice = Invoice.where(request.query_parameters).order(:id)
+    else
+      invoice = Invoice.find(params[:id])
+    end
+    render json: InvoiceSerializer.new(invoice)
+  end
 end
