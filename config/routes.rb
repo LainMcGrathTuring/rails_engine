0aeb1e3f49bf196e851c96503dcfdfa045ed7ca1
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-30 16:18:34 -0700

    Add index routes for transactions, items, and invoice_items

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+
       resources :items, only: [:index, :show] do
         resources :invoice_items, only: [:index]
         get '/merchant', to: 'merchants#show'
       end
 
-      resources :invoices, only: [:index, :show]
+      resources :invoices, only: [:index, :show] do
+        resources :transactions, only: [:index]
+        resources :items, only: [:index]
+        resources :invoice_items, only: [:index]
+      end
+
       resources :invoice_items, only: [:index, :show]
       resources :customers, only: [:index, :show] do
         resources :invoices, only: [:index]
