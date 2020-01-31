e9e9486869f78a485f2e48805fadc1c1306ebbfe
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-30 22:27:14 -0700

    move render json to return line

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -3,13 +3,14 @@ class Api::V1::InvoicesController < ApplicationController
   def index
     if params[:merchant_id]
       merchant = Merchant.find(params[:merchant_id])
-      render json: InvoiceSerializer.new(merchant.invoices.all)
+      invoices = merchant.invoices.all
     elsif params[:customer_id]
       customer = Customer.find(params[:customer_id])
-      render json: InvoiceSerializer.new(customer.invoices.all)
+      invoices = customer.invoices.all
     else
-      render json: InvoiceSerializer.new(Invoice.all)
+      invoices = Invoice.all
     end
+    render json: InvoiceSerializer.new(invoices)
   end
 
   def show
@@ -18,6 +19,9 @@ class Api::V1::InvoicesController < ApplicationController
     elsif params[:transaction_id]
       transaction = Transaction.find_by(id: params[:transaction_id])
       invoice = Invoice.find_by(id: transaction.invoice_id)
+    elsif params[:invoice_item_id]
+      invoice_item = InvoiceItem.find_by(id: params[:invoice_item_id])
+      invoice = Invoice.find_by(id: invoice_item.invoice_id)
     elsif params[:id] == "find_all"
       invoice = Invoice.where(request.query_parameters).order(:id)
     else
