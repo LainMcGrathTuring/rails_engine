c1078719d292946038507676246bcf0b55b1b2b9
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-01 11:35:32 -0700

    Add serializer for ids in factories

--- spec/factories/invoice.rb
+++ spec/factories/invoice.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
   factory :invoice do
-    id { 1 }
-    customer_id { 2 }
-    merchant_id { 2 }
+    sequence(:id) { |n| n }
     status { "shipped" }
+
+    created_at { "2012-03-27 14:53:59" }
+    updated_at { "2012-03-27 14:53:59" }
+    merchant
+    customer
   end
 end
--- spec/factories/merchant.rb
+++ spec/factories/merchant.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :merchant do
-    id { 2 }
+    sequence(:id) { |n| n }
     name { "Schroeder-Jerde" }
+    created_at { "2012-03-27 14:53:59" }
+    updated_at { "2012-03-27 14:53:59" }
   end
 end
