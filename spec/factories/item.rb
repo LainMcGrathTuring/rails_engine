11690399765b459b08bbefe04cc53c4cd5eb61c9
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-01 10:04:16 -0700

    add created and updated at to factory

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -5,5 +5,7 @@ FactoryBot.define do
     description { "Non id aperiam sunt aut distinctio non quia. Placeat dignissimos nisi atque impedit officiis. Odio aliquam rem magni. A doloribus magni magnam nobis qui iste. A odio unde tenetur." }
     unit_price { "387.16" }
     merchant_id { 2 }
+    created_at { "2012-03-27 14:53:59" }
+    updated_at { "2012-03-27 14:53:59" }
   end
 end
