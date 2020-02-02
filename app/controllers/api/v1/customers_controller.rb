d4753017df9a1016282518866537fbb477b207ca
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 12:55:26 -0700

    Refactor customers controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,16 +1,7 @@
 class Api::V1::CustomersController < ApplicationController
 
   def show
-    if params[:id] == "find"
-      customer = Customer.find_by(request.query_parameters)
-    elsif params[:id] == "find_all"
-      customer = Customer.where(request.query_parameters)
-    elsif params[:invoice_id]
-      invoice = Invoice.find_by(id: params[:invoice_id])
-      customer = Customer.find_by(id: invoice.customer_id)
-    else
-      customer = Customer.find(params[:id])
-    end
+    customer = Customer.find(params[:id])
     render json: CustomerSerializer.new(customer)
   end
 
