9edd243914aa540932249f8f4fa95e3fb4d1900f
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 12:57:03 -0700

    Refactor merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,19 +1,7 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def show
-    if params[:id] == "find"
-      merchant = Merchant.find_by(request.query_parameters)
-    elsif params[:id] == "find_all"
-      merchant = Merchant.where(request.query_parameters)
-    elsif params[:item_id]
-      item = Item.find_by(id: params[:item_id])
-      merchant = Merchant.find_by(id: item.merchant_id)
-    elsif params[:invoice_id]
-      invoice = Invoice.find_by(id: params[:invoice_id])
-      merchant = Merchant.find_by(id: invoice.merchant_id)
-    else
-      merchant = Merchant.find(params[:id])
-    end
+    merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.new(merchant)
   end
 
