926576cc9aa19d858b20719841d58290179f86a7
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 15:00:45 -0700

    convert unit_price to string

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -4,5 +4,9 @@ class InvoiceItemSerializer
   belongs_to :item
   belongs_to :invoice
 
-  attributes :id, :invoice_id, :item_id, :quantity, :unit_price
+  attributes :id, :invoice_id, :item_id, :quantity
+
+  attributes :unit_price do | unit |
+    unit.unit_price.to_s
+  end
 end
