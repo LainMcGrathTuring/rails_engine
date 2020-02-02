09f8026591b53b864ce7b32f0de3e80131c91062
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 14:56:12 -0700

    refactor show method

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::InvoiceItemsController < ApplicationController
 
   def show
-    if params[:id] == "find"
-      invoice_item = InvoiceItem.find_by(request.query_parameters)
-    elsif params[:id] == "find_all"
-      invoice_item = InvoiceItem.where(request.query_parameters).order(:id)
-    else
-      invoice_item = InvoiceItem.find(params[:id])
-    end
+    invoice_item = InvoiceItem.find(params[:id])
     render json: InvoiceItemSerializer.new(invoice_item)
   end
 
