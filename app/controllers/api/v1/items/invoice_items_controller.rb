7dca9b3c3f43bf961d6724b41916465ddfc8d03e
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 14:57:19 -0700

    refactor index method

--- app/controllers/api/v1/items/invoice_items_controller.rb
+++ app/controllers/api/v1/items/invoice_items_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::Items::InvoiceItemsController < ApplicationController
 
   def index
-    require "pry"; binding.pry
+    item = Item.find_by(id: params['item_id'])
+    invoice_item = InvoiceItem.where(item_id: item.id)
+    render json: InvoiceItemSerializer.new(invoice_item)
   end
 end
