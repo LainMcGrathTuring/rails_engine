b3688c0dad2026842d90d4e5fadd71fc39438485
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 12:56:08 -0700

    Refactor invoice_items_controller

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -12,14 +12,7 @@ class Api::V1::InvoiceItemsController < ApplicationController
   end
 
   def index
-    if params[:item_id]
-      item = Item.find(params[:item_id])
-      invoice_items = InvoiceItem.where(item_id: item.id)
-    elsif params[:invoice_id]
-      invoice_items = InvoiceItem.where(invoice_id: params[:invoice_id])
-    else
-      invoice_items = InvoiceItem.all
-    end
+    invoice_items = InvoiceItem.all
     render json: InvoiceItemSerializer.new(invoice_items)
   end
 end
