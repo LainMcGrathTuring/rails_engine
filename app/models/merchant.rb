15d0b4b70bc37009460772a9bea7aad1823acbfb
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 12:59:14 -0700

    Add most revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,15 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+
+  def self.most_revenue(number_of_records)
+    #merchant to invoices
+    #invoices to invoice_items, invoices to transactions
+    Merchant.joins(invoices: [:invoice_items, :transactions]).
+    select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS total_revenue').
+    group(:id).
+    merge(Transaction.successful).
+    order('total_revenue desc').
+    limit(number_of_records)
+  end
 end
