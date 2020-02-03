3f90a61d6382fb4f5d2f3d5685bd2fa9e6dfa2c3
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 23:10:53 -0700

    add invoice_items throguh invoices

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :merchant
 
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+
   def self.query_find(query_parameters)
     if query_parameters.keys == ["created_at"] || query_parameters.keys == ["updated_at"]
       item = Item.where(query_parameters).order(:id).first
     else
       item = Item.find_by(query_parameters)
     end
+  end
+
+  def self.total_number_sold(quantity)
+    Item.joins(invoices: [:invoice_items, :transactions]).
+    select('items.*, sum(invoice_items.quantity) AS total_sold').
+    group(:id).
+    merge(Transaction.successful).
+    order('total_sold desc').
+    limit(quantity)
   end
 end
