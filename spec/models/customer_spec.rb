dac119bfd365d1c71ab52e5a2adb1d79a0e18038
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 21:21:21 -0700

    add customer_merchant_transactions test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,23 +5,32 @@ RSpec.describe Customer do
     it { should have_many :invoices }
   end
 
-  it "can find favorite customer associated with a merchant" do
-    merchant_1 = create(:merchant)
-    customer_1 = create(:customer)
-    customer_2 = create(:customer)
-    customer_3 = create(:customer)
-    item_1 = create(:item, merchant_id: merchant_1.id)
-    item_2 = create(:item, merchant_id: merchant_1.id)
-    item_3 = create(:item, merchant_id: merchant_1.id)
-    item_4 = create(:item, merchant_id: merchant_1.id)
+  describe "customer model methods" do
 
-    invoice = create(:invoice, customer_id: customer_2.id, merchant_id: merchant_1.id)
+  before(:each) do
+    @merchant_1 = create(:merchant)
+    customer_1 = create(:customer)
+    @customer_2 = create(:customer)
+    customer_3 = create(:customer)
+    item_1 = create(:item, merchant_id: @merchant_1.id)
+    item_2 = create(:item, merchant_id: @merchant_1.id)
+    item_3 = create(:item, merchant_id: @merchant_1.id)
+    item_4 = create(:item, merchant_id: @merchant_1.id)
+
+    invoice = create(:invoice, customer_id: @customer_2.id, merchant_id: @merchant_1.id)
     transaction = create(:transaction, invoice_id: invoice.id)
 
-    merchant_1.items.each do | item |
+    @merchant_1.items.each do | item |
       create(:invoice_item, invoice_id: invoice.id)
     end
+  end
 
-    expect(Customer.merchant_transactions(merchant_1.id)).to eq([customer_2])
+    it "can find favorite customer associated with a merchant" do
+      expect(Customer.merchant_transactions(@merchant_1.id)).to eq([@customer_2])
+    end
+
+    it "can find a favorite merchant associated with a customer" do
+      expect(Customer.customer_merchant_transactions(@customer_2.id)).to eq([@merchant_1])
+    end
   end
 end
