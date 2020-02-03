269376b395ae0a855bbaa419e73aafbf9808de49
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-03 07:32:34 -0700

    move favorite customer controller

--- app/controllers/api/v1/customers/favorite_customer_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::Customers::FavoriteCustomerController < ApplicationController
-
-  def index
-    customer = Customer.merchant_transactions(params['merchant_id'].to_i).first
-    render json: CustomerSerializer.new(customer)
-  end
-end
