5259ffce9f5554dbe93c4a76642239d19c0c582f
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 19:00:34 -0700

    Remove commented out lines from most_revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,14 +2,31 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
 
+
   def self.most_revenue(number_of_records)
-    #merchant to invoices
-    #invoices to invoice_items, invoices to transactions
     Merchant.joins(invoices: [:invoice_items, :transactions]).
     select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS total_revenue').
     group(:id).
     merge(Transaction.successful).
     order('total_revenue desc').
     limit(number_of_records)
+  end
+
+  def self.total_items_sold(number_of_records)
+    Merchant.joins(invoices: [:invoice_items, :transactions]).
+    select('merchants.*, sum(invoice_items.quantity) AS total_sold').
+    group(:id).
+    merge(Transaction.successful).
+    order('total_sold desc').
+    limit(number_of_records)
+  end
+
+  def self.total_revenue(merchant_id)
+    merchant =  Merchant.joins(invoices: [:invoice_items, :transactions]).
+      select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS total_revenue').
+      group(:id).
+      merge(Transaction.successful).
+      where(id: merchant_id)
+    merchant.first
   end
 end
