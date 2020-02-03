436ca9f3d7ffdd537d2161a1b2e1453d3f57ecfb
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-02 18:59:37 -0700

    Add merchant transactions method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,13 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def self.merchant_transactions(merchant_id)
+      Customer.joins(invoices: :transactions).
+      select('customers.*, count(transactions.id) as transactions').
+      group(:id).
+      merge(Transaction.successful).
+      merge(Invoice.merchant(merchant_id)).
+      order('transactions desc').
+      limit(1)
+    end
 end
